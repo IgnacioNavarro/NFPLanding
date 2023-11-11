96bb26862e2a91653568062c782c7852038a42b1
Ignacio Navarro <inavarroblazquez@gmail.com>
2023-11-11 01:48:44 +0100

    added analytics

--- nfp/src/app/layout.js
+++ nfp/src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +12,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body>{children}</body>
+      <body>{children} 
+        <Analytics /> </body>
     </html>
   )
 }
